98e5675b792607776261fc679556ef0baa9f03a8
toantk <toantk.89@gmail.com>
2022-05-20 12:05:24 +0700

    Update ScratchCard.cs

--- GateSDK/sdk/ScratchCard.cs
+++ GateSDK/sdk/ScratchCard.cs
@@ -12,60 +12,70 @@ namespace vn.gate.sdk
     {
         protected SDKCoreKit sdkCoreKit;
 
-    /**
-     *
-     * @return SDKCoreKit
-     */
-    public SDKCoreKit getSdkCoreKit() {
-        return sdkCoreKit;
-    }
-
-    /**
-     *
-     * @param sdkCoreKit
-     * @throws InvalidArgumentException
-     */
-    public void setSdkCoreKit(SDKCoreKit sdkCoreKit) {
-        this.sdkCoreKit = assureApi(sdkCoreKit);
-    }
-
-    public PayCard() {
-        this.sdkCoreKit = assureApi(null);
-    }
-
-    /**
-     * @param SDKCoreKit|null instance
-     * @return SDKCoreKit
-     * @throws InvalidArgumentException
-     */
-    protected static SDKCoreKit assureApi(SDKCoreKit instance) {
-        instance = instance != null ? instance : SDKCoreKit.instance();
-        if (instance == null) {
-            throw new InvalidArgumentException("An Api instance must be provided as argument or set as instance in the SDKCoreKit");
+        /**
+         *
+         * @return SDKCoreKit
+         */
+        public SDKCoreKit getSdkCoreKit()
+        {
+            return sdkCoreKit;
         }
-        return instance;
-    }
 
-    /**
-     * @return string
-     */
-    protected String getEndpoint() {
-        return "scratchcard";
-    }
+        /**
+         *
+         * @param sdkCoreKit
+         * @throws InvalidArgumentException
+         */
+        public void setSdkCoreKit(SDKCoreKit sdkCoreKit)
+        {
+            this.sdkCoreKit = assureApi(sdkCoreKit);
+        }
 
-    public IResponseInterface claimScratchCard(Dictionary<String, Object> parameters) {
-        //validate data input
+        public PayCard()
+        {
+            this.sdkCoreKit = assureApi(null);
+        }
 
-        foreach (KeyValuePair<String, Object> entry in parameters) {
-            if (!ScratchCardFields.getEnumFields().Contains(entry.Key)) {
-                throw new InvalidArgumentException(entry.Key + " is not a field of " + "ScratchCardFields");
-            }            
-            if (entry.Value == null) {
-                throw new InvalidArgumentException(entry.Key + " field must be set");
+        /**
+         * @param SDKCoreKit|null instance
+         * @return SDKCoreKit
+         * @throws InvalidArgumentException
+         */
+        protected static SDKCoreKit assureApi(SDKCoreKit instance)
+        {
+            instance = instance != null ? instance : SDKCoreKit.instance();
+            if (instance == null)
+            {
+                throw new InvalidArgumentException("An Api instance must be provided as argument or set as instance in the SDKCoreKit");
             }
+            return instance;
         }
-        IResponseInterface response = this.getSdkCoreKit().call("/scratchcard", Request.METHOD_POST, parameters);
-        return response;
-    }
+
+        /**
+         * @return string
+         */
+        protected String getEndpoint()
+        {
+            return "scratchcard";
+        }
+
+        public IResponseInterface claimScratchCard(Dictionary<String, Object> parameters)
+        {
+            //validate data input
+
+            foreach (KeyValuePair<String, Object> entry in parameters)
+            {
+                if (!ScratchCardFields.getEnumFields().Contains(entry.Key))
+                {
+                    throw new InvalidArgumentException(entry.Key + " is not a field of " + "ScratchCardFields");
+                }
+                if (entry.Value == null)
+                {
+                    throw new InvalidArgumentException(entry.Key + " field must be set");
+                }
+            }
+            IResponseInterface response = this.getSdkCoreKit().call("/scratchcard", Request.METHOD_POST, parameters);
+            return response;
+        }
     }
 }
